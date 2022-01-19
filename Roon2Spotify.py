a8501e9e929298f2b7acb7c45403cb3f5afce6e7
Joe Cotellese <joe@cotellese.net>
2022-01-19 15:58:24 -0500

    old update

--- Roon2Spotify.py
+++ Roon2Spotify.py
@@ -15,6 +15,7 @@ my_client_id = env('SPOTIPY_CLIENT_ID')
 my_client_secret = env('SPOTIPY_CLIENT_SECRET')
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+spotify_token=""
 # Define retry util function
 # via Stack Overflow https://stackoverflow.com/questions/21786382/pythonic-way-of-retry-running-a-function
 # Define Exception class for retry
@@ -46,17 +47,14 @@ def retry_func(func, max_retry=10):
         raise RetryException('Failed to call function after retrying.', max_retry)
     
 def find_spotify(artist, album_name):
-    token = util.prompt_for_user_token("JoeCotellese","playlist-modify-private,user-library-modify",
-        client_id=my_client_id,client_secret=my_client_secret,redirect_uri='http://localhost')
-
-    sp = spotipy.Spotify(auth=token)
+    sp = spotipy.Spotify(auth=spotify_token)
     print ("searching for {}:{}".format(artist, album_name))
     search = "artist:{0} \"{1}]\"".format(artist, album_name).encode('utf-8')
-    result = retry_func(partial(sp.search, q=search, type='album', limit=1), max_retry=3)
+    result = retry_func(partial(sp.search, q=search, type='album', limit=1), max_retry=1)
     items = result['albums']['items']
     try:
         id = items[0]['id']
-        retry_func(partial(sp.current_user_saved_albums_add, albums=[id,]), max_retry=3)
+        retry_func(partial(sp.current_user_saved_albums_add, albums=[id,]), max_retry=1)
     except IndexError:
         print ("Error search for {} {}".format(artist, album_name))
     except RetryException as e:
@@ -76,13 +74,30 @@ def populate_albums(file):
 
 def populate_playlists(file):
     pass
+def flush_library():
+    logger.info("Flushing Library")
+    sp = spotipy.Spotify(auth=spotify_token)
+    
+    results = sp.current_user_saved_albums()
+    albums = results['items']
+    for album in albums:
+        tracks = sp.album_tracks(album['id'])
+        sp.current_user_saved_tracks_delete(tracks)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Populate Spotify from your Roon Library.")
     parser.add_argument('-a', '--album', dest='albums')
     parser.add_argument('-p', '--playlist', dest='playlists' )
+    parser.add_argument('--flush', default=False, action="store_true")
     file = "test_file.xlsx"
+    spotify_token = util.prompt_for_user_token("JoeCotellese",
+                                    "playlist-modify-private,user-library-modify,user-library-read",
+                                    client_id=my_client_id,
+                                    client_secret=my_client_secret,
+                                    redirect_uri='http://localhost')
 
     args = parser.parse_args()
+    if args.flush == True:
+        flush_library()
     if args.albums:
         populate_albums(args.albums)
